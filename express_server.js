208f9900236b4c49100e4effad8c32f3706a8a77
Friies <bahram.parsa2019@gmail.com>
2023-04-10 22:39:43 -0400

    add redirect a POST req for id-longURL to /urls.

--- express_server.js
+++ express_server.js
@@ -3,21 +3,29 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.use(express.urlencoded({ extended: true }));
+function generateRandomString() {
+  return Math.random().toString(36).substring(6);
+};
 
-function generateRandomString() {};
+app.post("/urls", (req, res) => {
+  const longURL = req.body.longURL;
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = longURL;
+  res.redirect(`/urls/${shortURL}`);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls", (req, res) => { // http://localhost:8080/urls
+app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
@@ -28,21 +36,22 @@ app.get("/urls/new", (req, res) => {
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = {
-    id: req.params.id, longURL: urlDatabase[req.params.id],
+    id: req.params.id, 
+    longURL: urlDatabase[req.params.id],
   };
   res.render("urls_show", templateVars);
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console.
-  res.send("OK"); // Respond with 'OK' (we will replace this)
-})
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
+});
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</body></html>\n");
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`)
+  console.log(`Example app listening on port ${PORT}!`);
 });
 
