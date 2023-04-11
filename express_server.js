f1492916b9a29c64dea8a967edcec10426f2ee12
Friies <bahram.parsa2019@gmail.com>
2023-04-11 00:58:36 -0400

    Add an update, submit and edit form to urls_show.ejs & urls_index.ejs. Allowing for updating of a urls and navigating to urls_show page. Add POST route to /urls/:id in express_server.js & fix minor errors.

--- express_server.js
+++ express_server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { url } = require("inspector");
 const app = express();
 const PORT = 8080; // default port 8080
 
@@ -13,13 +14,6 @@ const urlDatabase = {
 function generateRandomString() {
   return Math.random().toString(36).substring(6);
 };
-
-app.post("/urls", (req, res) => {
-  const longURL = req.body.longURL;
-  const shortURL = generateRandomString();
-  urlDatabase[shortURL] = longURL;
-  res.redirect(`/urls/${shortURL}`);
-});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -45,6 +39,30 @@ app.get("/urls/:id", (req, res) => {
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
+});
+
+app.post("/urls", (req, res) => {
+  const longURL = req.body.longURL;
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = longURL;
+  res.redirect(`/urls/${shortURL}`);
+});
+
+app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect("/urls");
+});
+
+app.post("/urls/:id", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = req.params.longURL;
+  urlDatabase[shortURL] = longURL;
+  
+  res.redirect("/urls");
+});
+
+app.post("/urls/:id/login", (req, res) => {
+  res.cookie("username", "");
 });
 
 app.get("/hello", (req, res) => {
