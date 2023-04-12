b9214862b8eac73aa68996b655570d7036f351ec
Friies <bahram.parsa2019@gmail.com>
2023-04-12 13:45:41 -0400

    Create file & template form for registration. - Create GET /register endpoint that redirects to urls_register.ejs. - Fixed minor error along the way.

--- express_server.js
+++ express_server.js
@@ -1,48 +1,105 @@
 const express = require("express");
-// const { url } = require("inspector");
 const app = express();
 const cookieParser = require("cookie-parser");
 const PORT = 8080; // default port 8080
 
+//EJS TEMPLATE ENGINE
 app.set("view engine", "ejs");
 
+//MIDLEWARES
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
-function generateRandomString() {
-  return Math.random().toString(36).substring(6);
-
-};
+//DATABASES
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+const users = {
+  userRandomID: {
+    id: "User1ID",
+    email: "user1@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+
+  user2RandomID: {
+    id: "User2ID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+
+//FUNCTIONS
+function generateRandomString() {
+  return Math.random().toString(36).substring(6);
+}
+
+const addUser = (email, password) => {
+  let id = generateRandomString();
+  users[id] = {
+    id,
+    email,
+    password,
+  };
+  return id;
+};
+
+const checkEmail = (database, email) => {
+  for (const user in database) {
+    if (users[user]["email"] === email) {
+      return true;
+    }
+  }
+  return false;
+};
+
+//URL ROUTES
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+// get request registration endpoint
+app.get("/register", (req, res) => {
+  let templateVars = {
+    user: users[req.cookies.userID]
+  };
+  res.render("urls_register", templateVars);
+  res.redirect("/urls");
+});
+
+//Login endpoint
+app.post("/login", (req, res) => {
+  res.cookie("user_id", req.body.user_id);
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("user_id");
+  res.redirect("/urls");
 });
 
 app.get("/urls", (req, res) => {
   const templateVars = {
     urls: urlDatabase,
-    username: req.cookies["username"],
+    user: users[req.cookies.userID],
   };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  const templateVars = { 
-    username: req.cookies["username"]
+  const templateVars = {
+    user: users[req.cookies.userID]
   };
   res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = {
-    id: req.params.id, 
+    id: req.params.id,
     longURL: urlDatabase[req.params.id],
-    username: req.cookies["username"],
+    user: users[req.cookies.userID],
   };
   res.render("urls_show", templateVars);
 });
@@ -51,6 +108,7 @@ app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
 });
+
 
 app.post("/urls", (req, res) => {
   const longURL = req.body.longURL;
@@ -72,19 +130,30 @@ app.post("/urls/:id", (req, res) => {
   res.redirect("/urls");
 });
 
-app.post("/login", (req, res) => {
-  res.cookie("username", "req.body.username");
-  res.redirect("/urls");
+// register post request
+app.post("/register", (req, res) => {
+  const { emailEntered, passwordEntered } = req.body;
+
+  //Checks for password or email blank fields
+  if (!emailEntered && !passwordEntered) {
+    res.status = 400;
+    res.status(400).send("Please enter a valid email or password!");
+  }
+  // checks for already registered email.
+  if (checkEmail(users, emailEntered)) {
+    res.status = 400;
+    res.send("This email is already in use, login or sign up with a different email.");
+  } else {
+    const user_id = addUser(emailEntered, passwordEntered);
+    res.cookie("user_id", user_id);
+    console.log(users);
+    res.redirect("/urls");
+  }
 });
 
-app.post("/logout", (req, res) => {
-  res.clearCookie("username");
-  res.redirect("/urls");
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</body></html>\n");
-});
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</body></html>\n");
+// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
